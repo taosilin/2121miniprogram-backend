e8676fe6be01d95b829b1d545b0459ed8110bc08
taosilin <852321819@qq.com>
2020-11-09 18:58:25 +0800

    11.9

--- src/main/java/com/example/backend/model/Cart.java
+++ src/main/java/com/example/backend/model/Cart.java
@@ -12,6 +12,22 @@ public class Cart {
 
     private Integer num;
 
+    private String lensID;
+
+    private Double leftDegree;
+
+    private Double rightDegree;
+
+    private Integer interpupillary;
+
+    private Double leftAstigmatism;
+
+    private Double rightAstigmatism;
+
+    private Integer leftAxis;
+
+    private Integer rightAxis;
+
     public String getUserID() {
         return userID;
     }
@@ -42,5 +58,69 @@ public class Cart {
 
     public void setSpecID(String specID) {
         this.specID = specID;
+    }
+
+    public Double getLeftDegree() {
+        return leftDegree;
+    }
+
+    public void setLeftDegree(Double leftDegree) {
+        this.leftDegree = leftDegree;
+    }
+
+    public Double getRightDegree() {
+        return rightDegree;
+    }
+
+    public void setRightDegree(Double rightDegree) {
+        this.rightDegree = rightDegree;
+    }
+
+    public Integer getInterpupillary() {
+        return interpupillary;
+    }
+
+    public void setInterpupillary(Integer interpupillary) {
+        this.interpupillary = interpupillary;
+    }
+
+    public Double getLeftAstigmatism() {
+        return leftAstigmatism;
+    }
+
+    public void setLeftAstigmatism(Double leftAstigmatism) {
+        this.leftAstigmatism = leftAstigmatism;
+    }
+
+    public Double getRightAstigmatism() {
+        return rightAstigmatism;
+    }
+
+    public void setRightAstigmatism(Double rightAstigmatism) {
+        this.rightAstigmatism = rightAstigmatism;
+    }
+
+    public Integer getLeftAxis() {
+        return leftAxis;
+    }
+
+    public void setLeftAxis(Integer leftAxis) {
+        this.leftAxis = leftAxis;
+    }
+
+    public Integer getRightAxis() {
+        return rightAxis;
+    }
+
+    public void setRightAxis(Integer rightAxis) {
+        this.rightAxis = rightAxis;
+    }
+
+    public String getLensID() {
+        return lensID;
+    }
+
+    public void setLensID(String lensID) {
+        this.lensID = lensID;
     }
 }
