99463e3bd2ea8b8012e6322577679bae169ef87e
taosilin <852321819@qq.com>
2020-12-18 19:15:33 +0800

    12.18

--- src/main/java/com/example/backend/model/Refund.java
+++ src/main/java/com/example/backend/model/Refund.java
@@ -13,6 +13,8 @@ public class Refund {
 
     private String specID;
 
+    private String lensID;
+
     private String applicant;
 
     private Double amount;
@@ -28,6 +30,20 @@ public class Refund {
     private Timestamp updateTime;
 
     private String refundImage;
+
+    private Double leftDegree;
+
+    private Double rightDegree;
+
+    private Integer interpupillary;
+
+    private Double leftAstigmatism;
+
+    private Double rightAstigmatism;
+
+    private Integer leftAxis;
+
+    private Integer rightAxis;
 
     public String getRefundID() {
         return refundID;
@@ -123,5 +139,69 @@ public class Refund {
 
     public void setRefundImage(String refundImage) {
         this.refundImage = refundImage;
+    }
+
+    public String getLensID() {
+        return lensID;
+    }
+
+    public void setLensID(String lensID) {
+        this.lensID = lensID;
+    }
+
+    public Double getLeftDegree() {
+        return leftDegree;
+    }
+
+    public void setLeftDegree(Double leftDegree) {
+        this.leftDegree = leftDegree;
+    }
+
+    public Double getRightDegree() {
+        return rightDegree;
+    }
+
+    public void setRightDegree(Double rightDegree) {
+        this.rightDegree = rightDegree;
+    }
+
+    public Integer getInterpupillary() {
+        return interpupillary;
+    }
+
+    public void setInterpupillary(Integer interpupillary) {
+        this.interpupillary = interpupillary;
+    }
+
+    public Double getLeftAstigmatism() {
+        return leftAstigmatism;
+    }
+
+    public void setLeftAstigmatism(Double leftAstigmatism) {
+        this.leftAstigmatism = leftAstigmatism;
+    }
+
+    public Double getRightAstigmatism() {
+        return rightAstigmatism;
+    }
+
+    public void setRightAstigmatism(Double rightAstigmatism) {
+        this.rightAstigmatism = rightAstigmatism;
+    }
+
+    public Integer getLeftAxis() {
+        return leftAxis;
+    }
+
+    public void setLeftAxis(Integer leftAxis) {
+        this.leftAxis = leftAxis;
+    }
+
+    public Integer getRightAxis() {
+        return rightAxis;
+    }
+
+    public void setRightAxis(Integer rightAxis) {
+        this.rightAxis = rightAxis;
     }
 }
