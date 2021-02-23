9fae39d0743abcfbfc7f55eee0667e885039d1ba
taosilin <852321819@qq.com>
2021-02-23 19:25:17 +0800

    2.23

--- src/main/java/com/example/backend/service/AddressService.java
+++ src/main/java/com/example/backend/service/AddressService.java
@@ -3,6 +3,7 @@ package com.example.backend.service;
 import com.example.backend.core.Service;
 import com.example.backend.model.Address;
 import java.util.List;
+
 public interface AddressService extends Service<Address>{
     void addAddress(Address a);
     void deleteByAddressID(String addressID);
